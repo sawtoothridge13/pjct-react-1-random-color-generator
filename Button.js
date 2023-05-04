7faa7bb0bd789ff6cc3e39ed8d091c1a7683c52c
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-04 15:00:45 +0200

    Add header, app container, input field, generate button

---
+++ Button.js
@@ -0,0 +1,1 @@
+const Button = <button>Generate</button>;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import './App.css';
-import logo from './logo.svg';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>Random Color Generator</div>
       </header>
+      <section>
+        <input />
+      </section>
+      <button>Generate</button>
     </div>
   );
 }
+
+/* onClick={() => {
+  // 3. Generate and set a new value for the emoji
+  const newEmoji = nodeEmoji.random().emoji;
+  setEmoji(newEmoji);
+}} */
