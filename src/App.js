f7a22a88f40649518029bde43ea3e350626c04d0
Doug Andrews <sawtoothridge13@gmail.com>
2023-05-04 18:52:17 +0200

    Add onClick event handler to button that changes text to random color hex code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ export default function App() {
   return (
     <div className="App">
       <header>Random Color Generator</header>
-      <span>Generated {color}</span>
+      <div>
+        <span>Generated {color}</span>
+      </div>
       <button onClick={() => setColor(randomColor)}>Generate</button>
     </div>
   );
